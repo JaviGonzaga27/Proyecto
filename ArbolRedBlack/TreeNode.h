a69702bf7155a0ecc4f5e7a7ce4f8ce532bb507c
Javier Gonzaga <www.javiier2015@hotmail.com>
2023-01-19 23:30:05 -0500

    Desarrollo de TreeNode

--- ArbolRedBlack/TreeNode.h
+++ ArbolRedBlack/TreeNode.h
@@ -1,1 +1,35 @@
 #pragma once
+
+enum class TreeNodeColor {
+    Black,
+    Red
+};
+
+template <typename T>
+class TreeNode {
+public:
+    TreeNode() = default;
+    explicit TreeNode(T value);
+
+    T value();
+    void value(T value);
+
+    TreeNode<T>*& left();
+    void left(TreeNode<T>* left);
+
+    TreeNode<T>*& right();
+    void right(TreeNode<T>* right);
+
+    TreeNode<T>*& parent();
+    void parent(TreeNode<T>* parent);
+
+    TreeNodeColor color();
+    void color(TreeNodeColor color);
+
+private:
+    T value_;
+    TreeNode<T>* left_ = nullptr;
+    TreeNode<T>* right_ = nullptr;
+    TreeNode<T>* parent_ = nullptr;
+    TreeNodeColor color_ = TreeNodeColor::Black;
+};
